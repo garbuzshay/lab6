3dbc7f411a49dd4f0efa1cd08bc7ce46f1a0ba00
garbuzshay <shay.garbuz@e.braude.ac.il>
2024-02-22 15:51:20 +0200

    first commit

---
+++ set_git.bat
@@ -0,0 +1,10 @@
+:: this bat file is used to set git repository
+:: to run use .\set_git.bat
+:: add :: before the next row to initialize the repository
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/garbuzshay/lab6.git
+git push -u origin main
